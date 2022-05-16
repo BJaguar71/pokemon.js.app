4e84925bd7eadc74f1644ef11f9f36ef4981d538
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-05-16 16:23:01 +0200

    changed the specific value type of the gender key to string

--- js/script.js
+++ js/script.js
@@ -3,30 +3,30 @@ let pokemonList = [
         name: 'Arbok',
         height: 3.5 + ' m',
         type: 'Poison',
-        gender: [50% + ' M ', 50% + ' F'] 
+        gender: ['50% M', '50% F'] 
     },
     {
       name: 'Kakuna',
       height: 0.6 + ' m',
       type: ['Bug', 'Poison'],
-      gender: [50% + ' M ', 50% + ' F']  
+      gender: ['50% M', '50% F']  
     },
     {
       name: 'Zubat',
       height: 0.8 + ' m',
       type: ['Poison', 'Flying'],
-      gender: [50% + ' M ', 50% + ' F'] 
+      gender: ['50% M', '50% F'] 
     },
     {
         name: 'Onix',
         height: 8.8 + ' m',
         type: ['Rock', 'Ground'],
-        gender: [50% + ' M ', 50% + ' F']
+        gender: ['50% M', '50% F']
     },
     {
         name: 'Kangaskhan',
         height: 2.2 + ' m',
         type: 'Normal',
-        gender: [0% + ' M', 100% + ' F']
+        gender: ['0% M', '100% F']
     }
 ];
