903fbbcb6d464d923870ef6fc6ff8f55c9b33398
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-05-16 10:26:39 +0200

    defined Kakuna pokemon object

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ let pokemonList = [
         type: 'Poison',
         gender: [50% + ' M ', 50% + ' F'] 
     },
+    {
+      name: 'Kakuna',
+      height: 0.6 + ' m',
+      type: ['Bug', 'Poison'],
+      gender: [50% + ' M ', 50% + ' F']  
+    },
