4e4ea7891703657db7c553f73080b641ba5c70d5
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-05-17 12:18:49 +0200

    changed gender's data type to the string

--- js/script.js
+++ js/script.js
@@ -3,30 +3,30 @@ let pokemonList = [
         name: 'Arbok',
         height: 3.5,
         type: 'Poison',
-        gender: [50% + ' M ', 50% + ' F'] 
+        gender: ['50% M', '50% F'] 
     },
     {
       name: 'Kakuna',
       height: 0.6,
       type: ['Bug', 'Poison'],
-      gender: [50% + ' M ', 50% + ' F']  
+      gender: ['50% M', '50% F']  
     },
     {
       name: 'Zubat',
       height: 0.8,
       type: ['Poison', 'Flying'],
-      gender: [50% + ' M ', 50% + ' F'] 
+      gender: ['50% M', '50% F'] 
     },
     {
         name: 'Onix',
         height: 8.8,
         type: ['Rock', 'Ground'],
-        gender: [50% + ' M ', 50% + ' F']
+        gender: ['50% M', '50% F']
     },
     {
         name: 'Kangaskhan',
         height: 2.2,
         type: 'Normal',
-        gender: [0% + ' M', 100% + ' F']
+        gender: ['0% M', '100% F']
     }
 ];
