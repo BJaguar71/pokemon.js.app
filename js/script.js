322664607221e3bedd68b14f53f03c995c171cca
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-05-17 12:18:14 +0200

    changed height's data type to the number

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 let pokemonList = [
     {
         name: 'Arbok',
-        height: 3.5 + ' m',
+        height: 3.5,
         type: 'Poison',
         gender: [50% + ' M ', 50% + ' F'] 
     },
     {
       name: 'Kakuna',
-      height: 0.6 + ' m',
+      height: 0.6,
       type: ['Bug', 'Poison'],
       gender: [50% + ' M ', 50% + ' F']  
     },
     {
       name: 'Zubat',
-      height: 0.8 + ' m',
+      height: 0.8,
       type: ['Poison', 'Flying'],
       gender: [50% + ' M ', 50% + ' F'] 
     },
     {
         name: 'Onix',
-        height: 8.8 + ' m',
+        height: 8.8,
         type: ['Rock', 'Ground'],
         gender: [50% + ' M ', 50% + ' F']
     },
     {
         name: 'Kangaskhan',
-        height: 2.2 + ' m',
+        height: 2.2,
         type: 'Normal',
         gender: [0% + ' M', 100% + ' F']
     }
