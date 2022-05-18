32fc2b442b0015c205119708074a66e04a13432c
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-05-18 11:45:17 +0200

    changed the "type" key-objects to the plural form

--- js/script.js
+++ js/script.js
@@ -2,31 +2,31 @@ let pokemonList = [
     {
         name: 'Arbok',
         height: 3.5,
-        type: 'Poison',
+        typs: 'Poison',
         gender: ['50% M', '50% F'] 
     },
     {
       name: 'Kakuna',
       height: 0.6,
-      type: ['Bug', 'Poison'],
+      typs: ['Bug', 'Poison'],
       gender: ['50% M', '50% F']  
     },
     {
       name: 'Zubat',
       height: 0.8,
-      type: ['Poison', 'Flying'],
+      typs: ['Poison', 'Flying'],
       gender: ['50% M', '50% F'] 
     },
     {
         name: 'Onix',
         height: 8.8,
-        type: ['Rock', 'Ground'],
+        typs: ['Rock', 'Ground'],
         gender: ['50% M', '50% F']
     },
     {
         name: 'Kangaskhan',
         height: 2.2,
-        type: 'Normal',
+        typs: 'Normal',
         gender: ['0% M', '100% F']
     }
 ];
