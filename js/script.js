5c8cc9cbac0407c6fc6b0e8597b10a5890637302
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-05-19 11:01:17 +0200

    corrected the "types"

--- js/script.js
+++ js/script.js
@@ -2,31 +2,31 @@ let pokemonList = [
     {
         name: 'Arbok',
         height: 3.5,
-        typs: ['Poison'],
+        types: ['Poison'],
         gender: ['50% M', '50% F'] 
     },
     {
       name: 'Kakuna',
       height: 0.6,
-      typs: ['Bug', 'Poison'],
+      types: ['Bug', 'Poison'],
       gender: ['50% M', '50% F']  
     },
     {
       name: 'Zubat',
       height: 0.8,
-      typs: ['Poison', 'Flying'],
+      types: ['Poison', 'Flying'],
       gender: ['50% M', '50% F'] 
     },
     {
         name: 'Onix',
         height: 8.8,
-        typs: ['Rock', 'Ground'],
+        types: ['Rock', 'Ground'],
         gender: ['50% M', '50% F']
     },
     {
         name: 'Kangaskhan',
         height: 2.2,
-        typs: ['Normal'],
+        types: ['Normal'],
         gender: ['0% M', '100% F']
     }
 ];
