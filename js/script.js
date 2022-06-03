6d46c6402f062ab31d8541fc8fd10efd53f156f3
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-06-03 13:40:19 +0200

    emptied the pokemon array

--- js/script.js
+++ js/script.js
@@ -1,35 +1,6 @@
 // a new variable defined for IIFE
 let pokemonRepository = (function (){
-  let pokemonList = [
-    {
-        name: 'Arbok',
-        height: 3.5,
-        types: ['Poison'],
-        gender: ['50% M', '50% F'] 
-    },
-    {
-      name: 'Kakuna',
-      height: 0.6,
-      types: ['Bug', 'Poison'],
-      gender: ['50% M', '50% F']  
-    },
-    {
-      name: 'Zubat',
-      height: 0.8,
-      types: ['Poison', 'Flying'],
-      gender: ['50% M', '50% F'] 
-    },
-    {
-        name: 'Onix',
-        height: 8.8,
-        types: ['Rock', 'Ground'],
-        gender: ['50% M', '50% F']
-    },
-    {
-        name: 'Kangaskhan',
-        height: 2.2,
-        types: ['Normal'],
-        gender: ['0% M', '100% F']
+  let pokemonList = [];
     }
 ]
 // function to add new pokemons
